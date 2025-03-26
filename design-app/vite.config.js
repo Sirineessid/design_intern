95663df89ccf5f57d2f006c88bd772a6783d6f35
Sirine Essid <sirineessid0@gmail.com>
2025-03-26 13:09:49 +0100

    Update vite.config.js

--- design-app/vite.config.js
+++ design-app/vite.config.js
@@ -2,7 +2,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
